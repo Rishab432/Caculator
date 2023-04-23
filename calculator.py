8b6aa9eb4f01a0c50123f847c5a70ba434134aff
Rishab432 <75451609+Rishab432@users.noreply.github.com>
2023-04-23 14:21:38 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,212 +1,173 @@
 import pygame
-import operator
-
-ops = {
-    '+' : operator.add,
-    '-' : operator.sub,
-    '*' : operator.mul,
-    '/' : operator.truediv,  # use operator.div for Python 2
-    '%' : operator.mod
-}
-
+import operator as op
 pygame.init()
 
-WIDTH = 280
-HEIGHT = 480
-SIZE = (WIDTH, HEIGHT)
 
-screen = pygame.display.set_mode(SIZE)
-clock = pygame.time.Clock()
+class Calculator:
+    def __init__(self) -> None:
+        self.screen = pygame.display.set_mode((360, 580))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.click = False
+        self.mx, self.my = pygame.mouse.get_pos()
 
-BLACK = (0, 0, 0)
-GREY = (67, 84, 90)
-WHITE = (255, 255, 255)
-ORANGE = (245, 153, 7)
-BLUE = (5, 80, 195)
+        self.equation = []
+        self.current_char = ''
+        self.solution = ''
+        self.ops = {
+            '^': op.pow,
+            '/': op.truediv,
+            '*': op.mul,
+            '-': op.sub,
+            '+': op.add
+        }
 
-x_offset = 0
-y_offset = 0
+        self.BLACK = (0, 0, 0)
+        self.GREY = (67, 84, 90)
+        self.WHITE = (255, 255, 255)
+        self.GOLD = (255, 215, 0)
+        self.CYAN = (0, 185, 185)
 
-font = pygame.font.SysFont(None, 40)
-largefont = pygame.font.SysFont(None, 70)
-def number_image():
-    display_num = font.render(str(num_value), True, WHITE)
-    screen.blit(display_num, (x_offset, y_offset))
-display_num0 = font.render("0", True, WHITE)
-display_val_plus = font.render("+", True, WHITE)
-display_val_minus = font.render("-", True, WHITE)
-display_val_multiply = font.render("*", True, WHITE)
-display_val_divide = font.render("/", True, WHITE)
-display_val_equals = font.render("=", True, WHITE)
-equation = ""
-values = []
-op1, op2, op3, op4, op5, op6, op7, op8, op9, op10, op11 = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
-num_of_operation = [op1, op2, op3, op4, op5, op6, op7, op8, op9, op10, op11]
-def total():
-    i = 0
-    op_number = 0
-    for i in len(values):
-        if values[i] == "+":
-            num_of_operation[op_number] = ops["+"]
-            op_number += 1
-def equation_comp():
-    i = 0
-    equation = ""
-    for i  in len(values):
-        equation += values[i]
-        i += 1
-    print(equation)
+        self.char_list = [
+            ['C', '[', ']', '/'],
+            ['7', '8', '9', '*'],
+            ['4', '5', '6', '-'],
+            ['1', '2', '3', '+'],
+            ['^', '0', '.', '='],
+        ]
+        self.button_list = [
+            [pygame.Rect(0, 130, 90, 90), pygame.Rect(90, 130, 90, 90), pygame.Rect(180, 130, 90, 90),
+             pygame.Rect(270, 130, 90, 90)],
+            [pygame.Rect(0, 220, 90, 90), pygame.Rect(90, 220, 90, 90), pygame.Rect(180, 220, 90, 90),
+             pygame.Rect(270, 220, 90, 90)],
+            [pygame.Rect(0, 310, 90, 90), pygame.Rect(90, 310, 90, 90), pygame.Rect(180, 310, 90, 90),
+             pygame.Rect(270, 310, 90, 90)],
+            [pygame.Rect(0, 400, 90, 90), pygame.Rect(90, 400, 90, 90), pygame.Rect(180, 400, 90, 90),
+             pygame.Rect(270, 400, 90, 90)],
+            [pygame.Rect(0, 490, 90, 90), pygame.Rect(90, 490, 90, 90), pygame.Rect(180, 490, 90, 90),
+             pygame.Rect(270, 490, 90, 90)]
+        ]
 
-def eval_equation():
-    print(total(values))
-number = ""
+        self.x_offset = 0
+        self.y_offset = 0
 
+        self.font = pygame.font.Font("digital-7.ttf", 65)
+        self.mini_font = pygame.font.Font("digital-7.ttf", 20)
 
-click = False
-running = True
-while running:
-    mx, my = pygame.mouse.get_pos()
+    def event_handler(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    self.click = True
 
-    screen.fill(WHITE)
+    def solver(self, equation_segment):
+        equation_segment = [character for character in equation_segment if character != '']
+        print(equation_segment)
+        for operator_type in self.ops:
+            for index, value in enumerate(equation_segment):
+                if value == operator_type:
+                    equation_segment[index - 1] = str(
+                        self.ops[operator_type](float(equation_segment[index - 1]), float(equation_segment[index + 1])))
+                    del equation_segment[index:index + 2]
+            print(equation_segment)
+        equation_segment = ''.join(equation_segment)
+        return equation_segment
 
-    for x_offset in range(0, 210, 70):
-        for y_offset in range(240, 480, 60):
-            pygame.draw.rect(screen, GREY, [x_offset, y_offset, 70, 60])
-            pygame.draw.rect(screen, WHITE, [x_offset, y_offset, 70, 60], 2)
-    for x_offset in range(0, 210, 140):
-        pygame.draw.rect(screen, BLUE, [x_offset, 420, 70, 60])
-        pygame.draw.rect(screen, WHITE, [x_offset, 420, 70, 60], 2)
-    for x_offset in range(0, 280, 70):
-        pygame.draw.rect(screen, BLUE, [x_offset, 180, 70, 60])
-        pygame.draw.rect(screen, WHITE, [x_offset, 180, 70, 60], 2)
-    for y_offset in range(240, 480, 60):
-        pygame.draw.rect(screen, ORANGE, [210, y_offset, 70, 60])
-        pygame.draw.rect(screen, WHITE, [210, y_offset, 70, 60], 2)
-    for x_offset in range(27, 220, 70):
-        for y_offset in range(257, 430, 60):
-            num_value = int(x_offset/70 + (y_offset-257)/20 + 1)
-            number_image()
-    screen.blit(display_num0, (97, 437))
-    screen.blit(display_val_plus, (display_val_plus.get_rect(center = screen.get_rect().center)[0]+105, 252))
-    screen.blit(display_val_minus, (display_val_plus.get_rect(center = screen.get_rect().center)[0]+107, 316))
-    screen.blit(display_val_multiply, (display_val_plus.get_rect(center = screen.get_rect().center)[0]+107, 382))
-    screen.blit(display_val_divide, (display_val_plus.get_rect(center = screen.get_rect().center)[0]+108, 437))
-    screen.blit(display_val_equals, (display_val_equals.get_rect(center = screen.get_rect().center)[0]+105, 192))
-    
-    number1 = pygame.Rect(0, 240, 70, 60)
-    number2 = pygame.Rect(70, 240, 70, 60)
-    number3 = pygame.Rect(140, 240, 70, 60)
-    number4 = pygame.Rect(0, 300, 70, 60)
-    number5 = pygame.Rect(70, 300, 70, 60)
-    number6 = pygame.Rect(140, 300, 70, 60)
-    number7 = pygame.Rect(0, 360, 70, 60)
-    number8 = pygame.Rect(70, 360, 70, 60)
-    number9 = pygame.Rect(140, 360, 70, 60)
-    number0 = pygame.Rect(70, 420, 70, 60)
-    val_plus = pygame.Rect(210, 240, 70, 60)
-    val_minus = pygame.Rect(210, 300, 70, 60)
-    val_multiply = pygame.Rect(210, 360, 70, 60)
-    val_divide = pygame.Rect(210, 420, 70, 60)
-    val_equals = pygame.Rect(210, 180, 70, 60)
+    def parser(self):
+        mirror = []
 
-    if number1.collidepoint((mx, my)):
-        if click:
-            if input_number_rect.left > 20:
-                print(1)
-                number += "1"
-    elif number2.collidepoint((mx, my)):
-        if click:
-            if input_number_rect.left > 20:
-                print(2)
-                number += "2"
-    elif number3.collidepoint((mx, my)):
-        if click:
-            if input_number_rect.left > 20:
-                print(3)
-                number += "3"
-    elif number4.collidepoint((mx, my)):
-        if click:
-            if input_number_rect.left > 20:
-                print(4)
-                number += "4"
-    elif number5.collidepoint((mx, my)):
-        if click:
-            if input_number_rect.left > 20:
-                print(5)
-                number += "5"
-    elif number6.collidepoint((mx, my)):
-        if click:
-            if input_number_rect.left > 20:
-                print(6)
-                number += "6"
-    elif number7.collidepoint((mx, my)):
-        if click:
-            if input_number_rect.left > 20:
-                print(7)
-                number += "7"
-    elif number8.collidepoint((mx, my)):
-        if click:
-            if input_number_rect.left > 20:
-                print(8)
-                number += "8"
-    elif number9.collidepoint((mx, my)):
-        if click:
-            if input_number_rect.left > 20:
-                print(9)
-                number += "9"
-    elif number0.collidepoint((mx, my)):
-        if click:
-            if input_number_rect.left > 20:
-                print(0)
-                number += "0"
-    elif val_plus.collidepoint((mx, my)):
-        if click:
-            print("+")
-            values.append(number)
-            # values.append(operator.add)
-            # eval_equation()
-    elif val_minus.collidepoint((mx, my)):
-        if click:
-            print("-")
-            values.append(number)
-    elif val_multiply.collidepoint((mx, my)):
-        if click:
-            print("*")
-            values.append(number)
-    elif val_divide.collidepoint((mx, my)):
-        if click:
-            print("/")
-            values.append(number)
-    elif val_equals.collidepoint((mx, my)):
-        if click:
-            print("=")
-            print(values)
-            equation_comp()
-    input_number = largefont.render(number, True, BLUE)
-    input_number_rect = input_number.get_rect()
-    input_number_rect.right = 275
-    input_number_rect.bottom = 100
-    screen.blit(input_number, (input_number_rect))
+        if "[" in self.equation:
+            open_list = []
+            current_open = 0
+            open_count = self.equation.count('[')
+            close_count = self.equation.count(']')
+            if open_count != close_count:
+                print('error, unmatched parenthesis')
+                return 'Error'
+            else:
+                for x in range(open_count):
+                    for y in range(len(self.equation)):
+                        if self.equation[y] == "[":
+                            open_list.append(y)
+                        elif self.equation[y] == "]":
+                            mirror.append([open_list[-1], y, Calculator.solver(self, self.equation[open_list[-1]+1:y])])
+                            self.equation[y] = Calculator.solver(self, self.equation[open_list[-1]+1:y])
+                            self.equation[open_list[-1]:y] = ['']*len(self.equation[open_list[-1]:y])
+                            del open_list[-1]
+                    # for replacer in mirror:
+                    #     if replacer[1] == -1:
+                    #         self.equation[replacer[0]] = replacer[2]
+                    #         del self.equation[replacer[0] + 1:]
+                    #     else:
+                    #         self.equation[replacer[0]] = replacer[2]
+                    #         del self.equation[replacer[0] + 1:replacer[1] + 1]
+                    #     mirror.remove(replacer)
+        self.current_char = Calculator.solver(self, self.equation)
+        return self.current_char
+
+    def interface(self):
+        while self.running:
+            self.mx, self.my = pygame.mouse.get_pos()
+            Calculator.event_handler(self)
+            self.screen.fill(self.WHITE)
+
+            string_equation = ''.join(self.equation)
+            disp_rect = pygame.draw.rect(self.screen, self.GOLD, (0, 0, 360, 130), 4, 8)
+            disp_chars = self.font.render(f"{self.current_char}", True, self.CYAN)
+            self.screen.blit(disp_chars, disp_chars.get_rect(centery=disp_rect.centery, right=disp_rect.right - 10))
+
+            for i in range(4):
+                for j in range(5):
+                    pygame.draw.rect(self.screen, self.CYAN, self.button_list[j][i], 0, 8)
+                    rectangle = pygame.draw.rect(self.screen, self.GOLD, self.button_list[j][i], 4, 8)
+                    character = self.font.render(f"{self.char_list[j][i]}", True, self.WHITE)
+                    self.screen.blit(character, character.get_rect(center=rectangle.center))
+                    if self.click and rectangle.collidepoint((self.mx, self.my)):
+                        if disp_chars.get_width() < 340 or self.char_list[j][i] in "^+-*/%C=":
+                            if self.char_list[j][i] == 'C':
+                                self.current_char = ''
+                                if len(self.equation) == 0:
+                                    print('error, no values in equation to clear')
+                                else:
+                                    self.equation = []
+                            elif self.char_list[j][i] == '=':
+                                if len(self.equation) == 0:
+                                    print('error, no values in equation to solve')
+                                else:
+                                    if self.current_char != '':
+                                        self.equation.append(self.current_char)
+                                    string_equation = ''.join(self.equation)
+                                    print(string_equation)
+                                    self.current_char = Calculator.parser(self)
+                                    print(self.current_char)
+                                    self.equation = []
+                            elif self.char_list[j][i] in "^+-*/%[]":
+                                if len(self.current_char) <= 0 and self.char_list[j][i] not in "[]":
+                                    print('error, no values to use operator on')
+                                else:
+                                    if self.current_char != '':
+                                        self.equation.append(self.current_char)
+                                    self.equation.append(self.char_list[j][i])
+                                    self.current_char = ''
+                            else:
+                                self.current_char += self.char_list[j][i]
+                        else:
+                            print("character limit reached")
+
+            disp_equation = self.mini_font.render(f"{string_equation}", True, self.CYAN)
+            self.screen.blit(disp_equation, disp_equation.get_rect(centery=20, right=disp_rect.right - 10))
+
+            self.click = False
+            pygame.display.update()
+            self.clock.tick(60)
+        pygame.quit()
 
 
-    click = False
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-        elif event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                click = True
-
-    pygame.display.flip()
-    clock.tick(30)
-
-pygame.quit()
-
-"""
-make the function check for open brackets and corresponding close brackets
-((3+2)/5)+(2+2)*2
-12   2  1 3   3
-"""
+if __name__ == "__main__":
+    run = Calculator()
+    run.interface()
